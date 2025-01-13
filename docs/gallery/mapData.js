c3bdd6b505f3a6f2d30aa3fffc7e8c17a2f27086
mdelorme-ch <mdelorme@mercator-ocean.fr>
2025-01-13 09:32:11 +0100

    Update mapData.js

--- docs/gallery/mapData.js
+++ docs/gallery/mapData.js
@@ -1,26 +1,26 @@
 const mapData = [
     {
         src: 'maps/Image1.png',
-        title: 'Global Shipping Traffic Density',
+        title: 'Global Shipping Traffic Density by Julian Hoffmann',
         alt: 'On the left: a map of Germany, showing the locations of all football clubs that ever played in Bundesliga. On the right: a horizontal bar chart with the top 18 clubs based on the total points gained: 1) Bayern München, 2) Borussia Dortmund, 3) Werder Bremen',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         
     },
     {
         src: 'maps/Image2.png',
-        title: 'Iceland & its glaciers',
+        title: 'Iceland & its glaciers by Julian Hoffmann',
         alt: 'A basemap of the coast of Belgium with the track of the Kusttram as a line and the stops as points.',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             },
     {
         src: 'maps/Image3.png',
-        title: 'Sea Surface Temperature',
+        title: 'Sea Surface Temperature by Chiara Phillips',
         alt: 'A map of Cologne showing the walking duration to the next REWE supermarket using areas - called isochrones - with different colours. It shows the locations of 85 REWE supermarkets in Cologne. For a significant proportion of the area, the next supermarket is within 15 minutes walking distance. ',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             },
      {
         src: 'https://github.com/mdelorme-ch/Dataviz-Webinar-Series/blob/main/docs/gallery/maps/Video1.gif?raw=true',
-        title: 'Global Shipping Traffic Density',
+        title: 'Global Shipping Traffic Density by Julian Hoffmann',
         alt: 'On the left: a map of Germany, showing the locations of all football clubs that ever played in Bundesliga. On the right: a horizontal bar chart with the top 18 clubs based on the total points gained: 1) Bayern München, 2) Borussia Dortmund, 3) Werder Bremen',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         
