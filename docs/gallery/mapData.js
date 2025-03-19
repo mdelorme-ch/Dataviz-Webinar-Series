a212b2d9eb9f2fd63a89126ff40e8a70752b2cc0
mdelorme-ch <mdelorme@mercator-ocean.fr>
2025-03-19 15:41:39 +0100

    Update mapData.js

--- docs/gallery/mapData.js
+++ docs/gallery/mapData.js
@@ -24,4 +24,10 @@ const mapData = [
         alt: 'On the left: a map of Germany, showing the locations of all football clubs that ever played in Bundesliga. On the right: a horizontal bar chart with the top 18 clubs based on the total points gained: 1) Bayern München, 2) Borussia Dortmund, 3) Werder Bremen',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         },
+    {
+        src: 'https://github.com/mdelorme-ch/Dataviz-Webinar-Series/blob/main/docs/gallery/maps/Phytoplankton-Bloom.gif?raw=true',
+        title: '2024 Spring Phytoplankton Bloom in North Atlanticby Chiara Phillips',
+        alt: 'On the left: a map of Germany, showing the locations of all football clubs that ever played in Bundesliga. On the right: a horizontal bar chart with the top 18 clubs based on the total points gained: 1) Bayern München, 2) Borussia Dortmund, 3) Werder Bremen',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        },
    ];
