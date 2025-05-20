aa6f03484f0ec7d1d7997c17a5457ef8bdcdbbaa
mdelorme-ch <mdelorme@mercator-ocean.fr>
2025-05-20 17:28:19 +0200

    Update mapData.js

--- docs/gallery/mapData.js
+++ docs/gallery/mapData.js
@@ -1,27 +1,49 @@
 const mapData = [
     {
         src: 'maps/Image2.png',
-        title: 'Iceland & its glaciers by Julian Hoffmann',
-        alt: 'A basemap of the coast of Belgium with the track of the Kusttram as a line and the stops as points.',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        title: 'Iceland & its glaciers by Julian Hoffmann Anton',
+        alt: 'Iceland & its glaciers by Julian Hoffmann Anton',
+        description: 'An experimental and creative map of Iceland with its 2019 main glacier contours. Render and viewport displays in Blender.',
             },
      {
         src: 'https://github.com/mdelorme-ch/Dataviz-Webinar-Series/blob/main/docs/gallery/maps/Video1.gif?raw=true',
-        title: 'Global Shipping Traffic Density by Julian Hoffmann',
-        alt: 'On the left: a map of Germany, showing the locations of all football clubs that ever played in Bundesliga. On the right: a horizontal bar chart with the top 18 clubs based on the total points gained: 1) Bayern München, 2) Borussia Dortmund, 3) Werder Bremen',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        title: 'Global Shipping Traffic Density by Julian Hoffmann Anton',
+        alt: 'Global Shipping Traffic Density by Julian Hoffmann Anton',
+        description: 'This is an experimental visualisation made in Blender with all observed ship movement from 2015-2020, at 500 m resolution from IMF's World Seaborne Trade Monitoring System & also NASA‘s Black Marble satellite imagery. Oceans and seas are transparent, creating a unique view that includes the other side of the globe.',
         },
     {
         src: 'maps/Image3.png',
         title: 'Sea Surface Temperature by Chiara Phillips',
-        alt: 'A map of Cologne showing the walking duration to the next REWE supermarket using areas - called isochrones - with different colours. It shows the locations of 85 REWE supermarkets in Cologne. For a significant proportion of the area, the next supermarket is within 15 minutes walking distance. ',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        alt: 'Sea Surface Temperature by Chiara Phillips',
+        description: 'Because the earth is a globe, maps have to distort the earth in some way to be presented on a 2D surface. Each of these "ways of distorting" are what we GIS-folk call "map projections". The projection shown here is the Spilhaus Projection, which is handy for seeing all of the world's oceans (ocean?) in one place. I love that this projection forces us to realize how connected the oceans really are. I created this map as part of the 2023 #30daymapchallenge.',
             },
     
     {
-        src: 'https://github.com/mdelorme-ch/Dataviz-Webinar-Series/blob/main/docs/gallery/maps/Phytoplankton-Bloom.gif?raw=true',
+        src: 'https://github.com/mdelorme-ch/Dataviz-Webinar-Series/blob/main/docs/gallery/maps/Phytoplankton_Bloom.gif?raw=true',
         title: '2024 Spring Phytoplankton Bloom in North Atlanticby Chiara Phillips',
         alt: '2024 Spring Phytoplankton Bloom in North Atlanticby Chiara Phillips',
         description: 'With the arrival of spring in the North Atlantic, longer days, calmer seas and nutriens stirred up by winter storms fuel massive phytoplankton blooms that spark a cascade of ocean life.',
         },
+
+    {
+        src: 'https://github.com/mdelorme-ch/Dataviz-Webinar-Series/blob/main/docs/gallery/maps/Phytoplankton_Bloom.gif?raw=true',
+        title: '2024 Spring Phytoplankton Bloom in North Atlanticby Chiara Phillips',
+        alt: '2024 Spring Phytoplankton Bloom in North Atlanticby Chiara Phillips',
+        description: 'With the arrival of spring in the North Atlantic, longer days, calmer seas and nutriens stirred up by winter storms fuel massive phytoplankton blooms that spark a cascade of ocean life.',
+        },    
+
+{
+        src: 'https://github.com/mdelorme-ch/Dataviz-Webinar-Series/blob/main/docs/gallery/maps/Phytoplankton_Bloom.gif?raw=true',
+        title: '2024 Spring Phytoplankton Bloom in North Atlanticby Chiara Phillips',
+        alt: '2024 Spring Phytoplankton Bloom in North Atlanticby Chiara Phillips',
+        description: 'With the arrival of spring in the North Atlantic, longer days, calmer seas and nutriens stirred up by winter storms fuel massive phytoplankton blooms that spark a cascade of ocean life.',
+        },  
+
+    {
+        src: 'https://github.com/mdelorme-ch/Dataviz-Webinar-Series/blob/main/docs/gallery/maps/Phytoplankton_Bloom.gif?raw=true',
+        title: '2024 Spring Phytoplankton Bloom in North Atlanticby Chiara Phillips',
+        alt: '2024 Spring Phytoplankton Bloom in North Atlanticby Chiara Phillips',
+        description: 'With the arrival of spring in the North Atlantic, longer days, calmer seas and nutriens stirred up by winter storms fuel massive phytoplankton blooms that spark a cascade of ocean life.',
+        },  
+    
    ];
