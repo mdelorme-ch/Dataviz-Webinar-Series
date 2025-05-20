351c41244b03a9ccba12e447f127e4c9ae93cb56
mdelorme-ch <mdelorme@mercator-ocean.fr>
2025-05-20 19:15:36 +0200

    Update mapData.js

--- docs/gallery/mapData.js
+++ docs/gallery/mapData.js
@@ -6,7 +6,7 @@ const mapData = [
         description: 'An experimental and creative map of Iceland with its 2019 main glacier contours. Render and viewport displays in Blender.',
             },
      {
-        src: 'https://mdelorme-ch/Dataviz-Webinar-Series/blob/main/docs/gallery/maps/Video1.gif?raw=true',
+        src: 'https://mdelorme-ch.github.io/Dataviz-Webinar-Series/gallery/maps/Video1.gif?raw=true',
         title: 'Global Shipping Traffic Density by Julian Hoffmann Anton',
         alt: 'Global Shipping Traffic Density by Julian Hoffmann Anton',
         description: 'This is an experimental visualisation made in Blender with all observed ship movement from 2015-2020, at 500 m resolution from IMF's World Seaborne Trade Monitoring System & also NASA‘s Black Marble satellite imagery. Oceans and seas are transparent, creating a unique view that includes the other side of the globe.',
@@ -19,42 +19,42 @@ const mapData = [
             },
     
     {
-        src: 'https://mdelorme-ch/Dataviz-Webinar-Series/blob/main/docs/gallery/maps/Phytoplankton_Bloom.gif?raw=true',
+        src: 'https://mdelorme-ch.github.io/Dataviz-Webinar-Series/gallery/maps/Phytoplankton_Bloom.gif?raw=true',
         title: '2024 Spring Phytoplankton Bloom in North Atlantic by Chiara Phillips',
         alt: '2024 Spring Phytoplankton Bloom in North Atlanticby Chiara Phillips',
         description: 'With the arrival of spring in the North Atlantic, longer days, calmer seas and nutriens stirred up by winter storms fuel massive phytoplankton blooms that spark a cascade of ocean life.',
         },
 
     {
-        src: 'https://mdelorme-ch/Dataviz-Webinar-Series/blob/main/docs/gallery/maps/Sea_surface_wind.gif?raw=true',
+        src: 'https://mdelorme-ch.github.io/Dataviz-Webinar-Series/gallery/maps/Sea_surface_wind.gif?raw=true',
         title: 'Global Ocean Sea Surface Wind by Julian Hoffmann Anton',
         alt: 'Global Ocean Sea Surface Wind by Julian Hoffmann Anton',
         description: 'Sea Surface Wind: January 10-12th 2025. Exploring innovative ways to display climate data of our beautiful planet.',
         },    
 
 {
-        src: 'https://mdelorme-ch/Dataviz-Webinar-Series/blob/main/docs/gallery/maps/ice-area.gif?raw=true',
+        src: 'https://mdelorme-ch.github.io/Dataviz-Webinar-Series/gallery/maps/ice-area.gif?raw=true',
         title: 'Arctic Sea Ice Extent by Lisa Kalacheva',
         alt: 'Arctic Sea Ice Extent by Lisa Kalacheva',
         description: 'This visualization compares Arctic sea ice in 1993 and 2023. An animated globe shows monthly ice extent for both years, highlighting a longer melt season in 2023. In 1993, melting peaked from August to October; in 2023, it extended from July to November. Additional graphs show decreased ice thickness and a 7–8% drop in December ice area. December was chosen as a representative month, avoiding the extremes of March and September.',
         },  
 
     {
-        src: 'https://mdelorme-ch/Dataviz-Webinar-Series/blob/main/docs/gallery/maps/3D_marine_heatwave.gif?raw=true',
+        src: 'https://mdelorme-ch.github.io/Dataviz-Webinar-Series/gallery/maps/3D_marine_heatwave.gif?raw=true',
         title: 'Vertical Extent of Marine Heatwaves in 2015-2016 by Shilpa Lal',
         alt: 'Vertical Extent of Marine Heatwaves in 2015-2016 by Shilpa Lal',
         description: 'This animation shows 3D vertical extent of Marine Heatwaves from 2015 to 2016, stressing on the February 2016 event. This 3D box is for the southwestern tropical Pacific.',
         },  
 
     {
-        src: 'https://mdelorme-ch/Dataviz-Webinar-Series/blob/main/docs/gallery/maps/marine_heatwave2016.gif?raw=true',
+        src: 'https://mdelorme-ch.github.io/Dataviz-Webinar-Series/gallery/maps/marine_heatwave2016.gif?raw=true',
         title: 'Marine Heatwave cumulative intensity beyond the surface by Shilpa Lal',
         alt: 'Vertical Extent of Marine Heatwaves in 2015-2016 by Shilpa Lal',
         description: 'The south western tropical Pacific experienced intense MHW activity in February 2016.The animation shows cumulative intensity of all the Marine Heatwaves events that peaked in February 2016 from surface to 1500m. For more information refer to Dutheil, C., Lal, S., Lengaigne, M., Cravatte, S., Menkès, C., Receveur, A., ... & Meier, H. M. (2024). The massive 2016 marine heatwave in the Southwest Pacific: An “El Niño–Madden-Julian Oscillation” compound event. Science Advances, 10(41), eadp2948.',
         },   
 
     {
-        src: 'https://mdelorme-ch/Dataviz-Webinar-Series/blob/main/docs/gallery/maps/ocean_habitats.gif?raw=true',
+        src: 'https://mdelorme-ch.github.io/Dataviz-Webinar-Series/gallery/maps/ocean_habitats.gif?raw=true',
         title: 'Our ocean habitats in decline by Hans Korving',
         alt: 'Our ocean habitats in decline by Hans Korving',
         description: 'We highlight Cod, Atlantic Herring and tropical corals, species living in very different parts of the ocean, from surface waters to the seafloor. Each reveals, in its own way, how changing ocean conditions impact marine life. By tracking environmental change in known habitat areas, we expose growing pressures over time. While this analysis simplifies a complex reality, it reveals clear signals of stress on key species. The data come from the Copernicus Marine Service and global habitat maps.',
